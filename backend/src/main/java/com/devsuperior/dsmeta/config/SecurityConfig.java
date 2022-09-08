5d3df261c20080bb390c8ab0ad4dae6aca768fcf
Jose Roberto <joseroberto.df@gmail.com>
2022-09-08 18:14:04 -0300

    Database

--- backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsmeta.config;
 
 import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
